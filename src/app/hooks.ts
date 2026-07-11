82f10f02925c7e79eb0dca672afb0e20c5149ee1
felixxplore <felixxplore08@gmail.com>
2026-07-11 11:57:45 +0530

    start with fresh ui

--- src/app/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from './store';
-
-// Use this instead of plain `useDispatch`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
